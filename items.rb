2911755e0c5bcfef86780a351340285f3685e39e
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-16 22:19:02 +0200

    final commit. should be all done

--- items.rb
+++ items.rb
@@ -1,26 +1,31 @@
 require_relative 'todolist.rb'
-
+load "todolist.rb"
 # Creates a new todo list
-list = ToDoList.new
+list = TodoList.new("Gymlife ToDo List")
 # Add four new items
 list.add_item("Go To The Gym")
 list.add_item("Enter The Gym Looking Badass")
 list.add_item("Realise You Aren't That Massive")
 list.add_item("Get Massive")
 # Print the list
-
+list.print_items_in_array 
 # Delete the first item
-
+list.delete_item(0)
 # Print the list
-
+list.print_items_in_array
 # Delete the second item
-
+list.delete_item(0)
 # Print the list
-
+list.print_items_in_array
 # Update the completion status of the first item to complete
-
+list.change_completed_status(0)
 # Print the list
-
+list.print_items_in_array
 # Update the title of the list
+list.update_title("New Awesome Todo List")
+#list.print_list 
+list.print_items_in_array
 
-# Print the list
+
+#feature
+list.check_for_list_length
--- todolist.rb
+++ todolist.rb
@@ -1,11 +1,12 @@
 class TodoList
 attr_reader :title, :items
-  def initialize(list_title)
+  
+  	def initialize(list_title)
         @title = list_title
         @items = Array.new # Starts empty! No Items yet!
-     end
+    end
 
-      def add_item(new_item)
+    def add_item(new_item)
      	item = Item.new(new_item)
      	@items.push(item)
     end
@@ -22,13 +23,49 @@ attr_reader :title, :items
    			puts "-"*17
    		end
    	end
+
+   	def change_completed_status(index)
+   		@items[index].completion_status_changed
+   	end
+   	
+   	def lort
+   		puts "#{@items[1].completed_status}"
+   	end
+
+    def delete_item(item_place)
+    	@items.delete_at(item_place)
+    end	
+
+    def update_title(new_title)
+    	@title = new_title
+    end
+
+    def check_for_list_length
+    	puts
+    	if @items.empty? == false
+    		puts "Your 'New Awesome Todo List' Is Unfortunately Not Finished..."
+    	else
+    		puts "Nice! All Done. Now, play WoW"
+    	end
+    end	
 end
 
 class Item
 	attr_reader :description, :completed_status
+	attr_writer :completed_status
 
 	def initialize(item_description)
 		@description = item_description
 		@completed_status = false
 	end
+
+	def print_each_item
+	    puts "#{@description}"
+		puts "#{completed_status}"
+		puts "-"*17
+    end
+
+    def completion_status_changed
+    	@completed_status = true
+    end
 end
