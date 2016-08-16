2644f7708b3fc309d2956e3a6106d05a46f30aaa
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-16 22:08:28 +0200

    second commit

--- items.rb
+++ items.rb
@@ -3,7 +3,10 @@ require_relative 'todolist.rb'
 # Creates a new todo list
 list = ToDoList.new
 # Add four new items
-
+list.add_item("Go To The Gym")
+list.add_item("Enter The Gym Looking Badass")
+list.add_item("Realise You Aren't That Massive")
+list.add_item("Get Massive")
 # Print the list
 
 # Delete the first item
