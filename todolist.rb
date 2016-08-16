82d58a549a376dd918eb6765936c5445ebb97ee9
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-16 21:45:17 +0200

    initial commit

---
+++ todolist.rb
@@ -0,0 +1,11 @@
+class TodoList
+
+  def initialize(list_title)
+        @title = list_title
+        @items = Array.new # Starts empty! No Items yet!
+     end
+end
+
+class Item
+    # methods and stuff go here
+end
